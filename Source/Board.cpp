39aea4af6bffed7b08c89a3842672de9592cdca9
Majloszka <milosz.mazur92@gmail.com>
2017-01-12 01:10:20 +0100

    Board: added constructor, getFields, isBoarBlankedAfterCreation

--- Source/Board.cpp
+++ Source/Board.cpp
@@ -1,1 +1,14 @@
+#include "Board.hpp"
+#include <algorithm>
+#include<vector>
 
+Board::Board()
+{
+    fields_.resize(6);
+    std::fill(fields_.begin(), fields_.end(), fieldType::Blank);
+}
+    
+std::vector<fieldType> Board::getFields()
+{
+    return fields_;
+}
--- Source/Board.hpp
+++ Source/Board.hpp
@@ -1,1 +1,18 @@
+#pragma once
+#include<vector>
 
+enum class fieldType
+{
+    X,
+    O,
+    Blank
+};
+
+class Board
+{
+private:
+    std::vector<fieldType> fields_;
+public:
+    Board();
+    std::vector<fieldType> getFields();
+};
